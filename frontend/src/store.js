efbcb2f786fe58adb706390f7099b8ef4fafc97c
Marco Accardi <contact.marco.accardi@gmail.com>
2020-11-26 17:47:06 +0100

    init redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,7 @@ const middleware = [thunk]
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware([...middleware])) // using thunk we can just use the Redux Dev Tools extension
+  composeWithDevTools(applyMiddleware(...middleware)) // using thunk we can just use the Redux Dev Tools extension
 )
 
 export default store
