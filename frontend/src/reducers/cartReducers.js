5057b47f68693f2535e7ad8d4f68a9bad575f589
Marco Accardi <contact.marco.accardi@gmail.com>
2020-11-27 08:55:15 +0100

    Cart Screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,10 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
-export const cartReducer = (state = { cartItem: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
-      const existItem = state.cartItem.find((x) => x.prodcuct === item.product)
+      const existItem = state.cartItems.find((x) => x.product === item.product)
       if (existItem) {
         return {
           ...state,
@@ -15,7 +15,7 @@ export const cartReducer = (state = { cartItem: [] }, action) => {
       } else {
         return {
           ...state,
-          cartItems: [...state.cartItem, item],
+          cartItems: [...state.cartItems, item],
         }
       }
     default:
