59e0fc4f72595b474cfb12ca6eab4cab5eb9c819
Marco Accardi <contact.marco.accardi@gmail.com>
2020-11-27 11:21:04 +0100

    generate json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
@@ -6,6 +7,7 @@ import User from '../models/userModel.js'
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
+
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
@@ -14,11 +16,30 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
     throw new Error('invalid email or password')
   }
 })
-export { authUser }
+
+// @desc    Auth user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+export { authUser, getUserProfile }
