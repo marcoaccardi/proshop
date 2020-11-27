4621b1b8dd3ba8de20e5fbf5586e73c594bd6a8c
Marco Accardi <contact.marco.accardi@gmail.com>
2020-11-27 09:44:30 +0100

    userAuthentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -12,15 +13,18 @@ connectDB()
 // INIT EXPRESS
 const app = express()
 
+// middleware to get the body
+app.use(express.json())
+
 // ROOT PAGE
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
